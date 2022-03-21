8417de1361891ecb9a1ae502af151aa7ce6f439b
Rafael <rafaelratier@hotmail.com>
2022-03-21 10:50:44 -0300

    Updates

---
+++ precache-manifest.ff9be1cbff712b0f1711cd16f0c8c126.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f49e2a025545892ef1990b856a625789",
+    "url": "/trivia/index.html"
+  },
+  {
+    "revision": "450cc5aa7a2aca7e92e9",
+    "url": "/trivia/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "bfbc0778c1b36178ac7f",
+    "url": "/trivia/static/css/main.6e94774b.chunk.css"
+  },
+  {
+    "revision": "450cc5aa7a2aca7e92e9",
+    "url": "/trivia/static/js/2.d2105acf.chunk.js"
+  },
+  {
+    "revision": "0003a4c9dbbb651d32794093500d6be6",
+    "url": "/trivia/static/js/2.d2105acf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfbc0778c1b36178ac7f",
+    "url": "/trivia/static/js/main.14efb16b.chunk.js"
+  },
+  {
+    "revision": "94ea6be6336a337623c7",
+    "url": "/trivia/static/js/runtime-main.c352ba8e.js"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/trivia/static/media/trivia.466d153e.png"
+  }
+]);
